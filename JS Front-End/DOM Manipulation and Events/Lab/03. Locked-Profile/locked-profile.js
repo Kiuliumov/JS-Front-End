2ee0130c60d731ca84b30e94e42917bc572e5f40
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-15 20:21:40 +0300

    JS Front-End

--- JS Front-End/DOM Manipulation and Events/Lab/03. Locked-Profile/locked-profile.js
+++ JS Front-End/DOM Manipulation and Events/Lab/03. Locked-Profile/locked-profile.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
-}
+    var flag = false;
+    
+    document.querySelectorAll('button').forEach((b) => {b.addEventListener('click', 
+        (e) => {
+           if(!e.target.disabled){
+                if(!flag){
+                    e.target.parentNode.querySelector('.hidden-fields.active').style.display = 'block';
+                    e.target.textContent = 'Show less';
+                }else{
+                    e.target.textContent = 'Show more';
+                    e.target.parentNode.querySelector('.hidden-fields.active').style.display = 'none';
+                }
+                flag = !flag;
+           }
+        }
+    )}
+)};
