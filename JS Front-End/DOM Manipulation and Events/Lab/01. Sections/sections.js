9d193ee8c6dbdc13340843df99d0571fc02f275a
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-15 20:21:35 +0300

    Update sections.js

--- JS Front-End/DOM Manipulation and Events/Lab/01. Sections/sections.js
+++ JS Front-End/DOM Manipulation and Events/Lab/01. Sections/sections.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const arrayOfStrings = document.querySelector('input[type="text"]').value.split(',');
+   const content = document.querySelector('#content');
+
+   arrayOfStrings.forEach(e=> {
+         const div = document.createElement('div');
+         const p = document.createElement('p');
+
+         p.textContent = e;
+         div.appendChild(p);
+         content.appendChild(div);
+   });
 }
