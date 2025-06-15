f5addca30b900bc56a8777140f87aa5eebf5926d
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-15 20:21:43 +0300

    Update fill-dropdown.js

--- JS Front-End/DOM Manipulation and Events/Lab/04. Fill-Dropdown/fill-dropdown.js
+++ JS Front-End/DOM Manipulation and Events/Lab/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const submit = document.querySelector('input[type="submit"]');
+
+    submit.addEventListener('click', (e) => {
+        const newItemTextInput = document.querySelector('#newItemText');
+        const newItemValInput = document.querySelector('#newItemValue');
+
+        const newItemText = newItemTextInput.value;
+        const newItemVal = newItemValInput.value;
+
+        if (newItemText && newItemVal) {
+            const option = document.createElement('option');
+            option.text = newItemText;
+            option.value = newItemVal;
+
+            const select = document.querySelector('select');
+            select.appendChild(option);
+
+            newItemTextInput.value = '';
+            newItemValInput.value = '';
+        }
+    });
 }
