7395efde5344cc88c69b52b9659bec654c760d3f
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-16 21:42:21 +0300

    Update distance-converter.js

--- JS Front-End/DOM Manipulation and Events/Lab/07. Distance-Converter/distance-converter.js
+++ JS Front-End/DOM Manipulation and Events/Lab/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,28 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+
+    const metricsDict = {
+        'km' : 1000,
+        'm' : 1,
+        'cm' : 0.01,
+        'mm' : 0.001,
+        'mi': 1609.34,
+        'yrd': 0.9144,
+        'ft': 0.3048,
+        'in': 0.0254
+    }
+
+
+    document.getElementById('convert').addEventListener('click', (e) => {
+            e.preventDefault();
+            const inputDistance = Number(document.getElementById('inputDistance').value);
+            const from = document.getElementById('inputUnits').value;
+            const fromInMeters = metricsDict[from] * inputDistance;
+            const to = document.getElementById('outputUnits').value;
+            const outputDistance = document.getElementById('outputDistance');
+            const output = fromInMeters / metricsDict[to];
+            outputDistance.value = output.toFixed(2);
+  
+    })
 }
